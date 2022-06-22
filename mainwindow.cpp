c45114f72989941f0e9fb0f7457a4b3b30ef4544
Edge2020 <wuzhenlin2013@live.com>
2022-06-22 21:00:59 +0800

    0.0.2

---
+++ mainwindow.cpp
@@ -0,0 +1,128 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowTitle("ExistOS Updater");
+    ui->label_version->setText(VERSION);
+    ui->output->setText("Connect your calculator to computer while pressing [ON/C] key.\nThen press FLASH button.");
+    if(edbINFO->isFile() == false || sbloaderINFO->isFile() == false){
+        QMessageBox::critical(this, tr("Error"), tr("Files are missing! Please reinstall this application."));
+        ui->button_flash->setEnabled(false);
+    }
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+    delete p1;
+    delete p2;
+    delete path_OSLoader;
+    delete path_System;
+    delete edbINFO;
+    delete sbloaderINFO;
+}
+
+void MainWindow::doStage(int stage){
+    if(stage == 1){
+        ui->output->clear();
+        addLine("Prepare to flash...");
+        QStringList argu;
+        argu.append("-f");
+        argu.append(*path_OSLoader);
+        openProcess(stage, QDir::currentPath() + "/sb_loader.exe", argu);
+    }else{
+        if(stage == 2){
+        addLine("Installing system...");
+        QStringList argu;
+        argu.append("-r");
+        argu.append("-s");
+        argu.append("-f");
+        argu.append(*path_OSLoader);
+        argu.append("1408");
+        argu.append("b");
+        argu.append("-f");
+        argu.append(*path_System);
+        argu.append("1984");
+        openProcess(stage, QDir::currentPath() + "/edb.exe", argu);
+        }
+    }
+}
+
+void MainWindow::openProcess(const int & stage, const QString & path, const QStringList & argu){
+    if(stage == 1){
+        p1->start(path, argu);
+        connect(p1, SIGNAL(finished(int)), this, SLOT(readResult(int)));
+    }else{
+        if(stage == 2){
+            p2->start(path, argu);
+            connect(p2, SIGNAL(finished(int)), this, SLOT(readResult(int)));
+        }
+    }
+}
+
+void MainWindow::addLine(const QString & texts){
+    ui->output->setText(ui->output->toPlainText() + texts + "\n");
+    ui->output->moveCursor(QTextCursor::End);
+}
+
+void MainWindow::readResult(int exitCode){
+    QByteArray result;
+    if(stage == 1){
+        result = p1->readAll();
+    }else{
+        if(stage == 2){
+            result = p2->readAll();
+        }
+    }
+
+    addLine(QString::fromLocal8Bit(result));
+    stage++;
+    if(stage <= 2){
+        doStage(stage);
+    }else{
+        addLine("Done!");
+        ui->button_flash->setEnabled(true);
+        stage = 1;
+        p1->close();
+        p2->close();
+    }
+}
+
+void MainWindow::on_button_OSLoader_path_clicked()
+{
+    ui->OSLoader_path->setText(QFileDialog::getOpenFileName(this, tr("Select a file"), tr(""), tr("OS Loader File(*.sb)")));
+}
+
+
+void MainWindow::on_button_System_path_clicked()
+{
+    ui->System_path->setText(QFileDialog::getOpenFileName(this, tr("Select a file"), tr(""), tr("System File(*.sys)")));
+}
+
+
+void MainWindow::on_button_flash_clicked()
+{
+    *path_OSLoader = ui->OSLoader_path->text();
+    *path_System = ui->System_path->text();
+    QFileInfo osloaderINFO;
+    QFileInfo systemINFO;
+    osloaderINFO.setFile(*path_OSLoader);
+    systemINFO.setFile(*path_System);
+    if(*path_OSLoader == "" || *path_System == ""){
+        QMessageBox::warning(this, tr("Warning"), tr("File(s) unselected!"));
+    }else{
+        if(osloaderINFO.isFile() && systemINFO.isFile()){
+            if(QMessageBox::question(this, tr("Question"), tr("Are you sure you want to update your calculator?")) == QMessageBox::Yes){
+                ui->button_flash->setEnabled(false);
+                doStage(stage);
+            }
+        }else{
+            QMessageBox::critical(this, tr("Error"), tr("File does not exist!"));
+        }
+    }
+}
+
---
+++ mainwindow.h
@@ -0,0 +1,47 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QProcess>
+#include <QDir>
+#include <QFileInfo>
+
+constexpr auto VERSION = "ver 0.0.2";
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void addLine(const QString & texts);
+    void openProcess(const int & stage, const QString & path, const QStringList & argu);
+    void doStage(int stage);
+    int stage = 1;
+
+private slots:
+    void on_button_OSLoader_path_clicked();
+
+    void on_button_System_path_clicked();
+
+    void on_button_flash_clicked();
+
+    void readResult(int exitCode);
+
+private:
+    Ui::MainWindow *ui;
+    QProcess* p1 = new QProcess(this);
+    QProcess* p2 = new QProcess(this);
+    QString* path_System = new QString;
+    QString* path_OSLoader = new QString;
+    QFileInfo* edbINFO = new QFileInfo(QDir::currentPath() + "/edb.exe");
+    QFileInfo* sbloaderINFO = new QFileInfo(QDir::currentPath() + "/sb_loader.exe");
+};
+#endif // MAINWINDOW_H
